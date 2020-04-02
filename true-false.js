244bb5690d9b1486b3072024ab82ba58e96b6091
tapuchandramojumder <mojumdertapu@gmail.com>
2020-04-02 21:40:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+const age = 14;
+if (age > 0) {
+  console.log('condition is true');
+} else {
+  console.log('condition is false');
+}
+
+const name = 'false';
+if (name.length > 0) {
+  console.log('condition is true');
+} else {
+  console.log('condition is false');
+}
+
+// summary of falsy value is: (0,"",undefined, null, NaN,false);
+//summary of truthy value is :("0","  ", [],{} ,)
