4522bcef644dde82cc7faf653a9328f6f1e932e5
tapuchandramojumder <mojumdertapu@gmail.com>
2020-04-03 01:59:54 +0600

    map-filter-find-reduce learned

---
+++ map-filter-find.js
@@ -0,0 +1,33 @@
+const numbers = [303, 4, 5, 6, 7, 8, 9];
+// const square = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   const element = numbers[i];
+//   const result = element * element;
+//  square.push(result);
+// }
+const square = numbers.map(element => element * element); // map have three parameters like(element,index,array) and map returns new array with the transformed elements
+
+console.log(square); //result:[91809,16,25,36,49,64,81]
+
+const bigger = numbers.filter(element => element > 5);
+console.log(bigger); //result:[303,6,7,8,9]
+
+const biggerOne = numbers.find(element => element > 5);
+//find provides just one element;
+console.log(biggerOne); //result:303
+
+const result = numbers.reduce((element, value) => element - value);
+//reducer takes four parameter(element, current Value,index,array)
+console.log(result); //result:264
+
+const number2 = [1, 29, 47];
+const numArr = [];
+number2.forEach(function(element) {
+  //forEach() method returns undefined
+  const todo = element + element;
+  numArr.push(todo);
+});
+console.log(numArr); //result:[2,58,94]
+
+//Q.A : forEach() Vs map() in javascript?
