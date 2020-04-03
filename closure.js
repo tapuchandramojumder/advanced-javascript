2b3dedd5206d630c4adcc7e98e2c53433bf7f574
tapuchandramojumder <mojumdertapu@gmail.com>
2020-04-03 12:17:09 +0600

    try to understand closure

---
+++ closure.js
@@ -0,0 +1,20 @@
+let l = 'L is a global scope variable';
+function OuterFun() {
+  let j = 'J is a outer variable';
+  console.log(l);
+  console.log(j);
+  //console.log(k); result: error ; because it doesn't exit OuterFun
+
+  function InnerFun() {
+    let k = 'K is a Inner variable';
+    console.log(k);
+    console.log(j);
+    console.log(l);
+  }
+  InnerFun();
+}
+OuterFun();
+
+
+//Q.A: what is closure?
+//Ans: If function  have Inside a function this inside function doesn't exits outer function variable but outer function exits inner function variable thats call closure;
